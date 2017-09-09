c7704cdbac51c77a49e643d07665b58d951a0e13
ekstazi <ekstazyst@gmail.com>
2017-09-09 13:38:44 +0300

    Initial structure

---
+++ migrations/m170909_103757_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170909_103757_users extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170909_103757_users cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170909_103757_users cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
